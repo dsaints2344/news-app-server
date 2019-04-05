d8d595caa8436e416d16ff6395febee1fd633bd7
David De Los Santos <ddelossantosmateo@gmail.com>
2019-04-05 07:15:15 -0400

    Trying to set up server

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require('express');
-const mssql = require('msql');
+const mssql = require('mssql');
 
 const app = express();
+
+const config = {
+    user: 'sa',
+    password: 'Password@_2362',
+    server:'localhost\\MSSQLSERVER',
+    database: 'Northwind'
+}
+
+app.get('/', function (req, res){
+    mssql.connect(config, function (error) {
+        if(error) console.log(error);
+       
+
+        var request = new mssql.Request();
+
+        request.query("SELECT * FROM Customers", function(err, recordset){
+            if (err) console.log(err);
+            res.send(JSON.stringify(recordset));
+        })
+    });
+
+    mssql.close();
+
+})
 
 app.listen(5000, () => {
     console.log('Server running on port 5000...')
