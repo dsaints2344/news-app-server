2cd38ee525c8bfe6d06f832e561086cf15207bf2
David De Los Santos <ddelossantosmateo@gmail.com>
2019-04-05 16:24:36 -0400

    Finishing setting up methods

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
+const mysql = require('mysql');
 const express = require('express');
-const mssql = require('mssql');
 
 const app = express();
 
-const config = {
-    user: 'sa',
-    password: 'Password@_2362',
-    server:'localhost\\MSSQLSERVER',
-    database: 'Northwind'
-}
-
-app.get('/', function (req, res){
-    mssql.connect(config, function (error) {
-        if(error) console.log(error);
-       
-
-        var request = new mssql.Request();
-
-        request.query("SELECT * FROM Customers", function(err, recordset){
-            if (err) console.log(err);
-            res.send(JSON.stringify(recordset));
-        })
-    });
-
-    mssql.close();
-
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:'Password@_2362',
+    database:'world'
 })
 
+connection.connect(err => {
+    if (err) {
+        return err;
+    }
+});
+
+app.get('/', (req, res) => {
+    connection.query("call create_table()", (err, results) =>{
+        if (err) {
+            res.send(err);
+        }
+        else(
+            console.log("datos introducidos")
+        )
+    });
+});
+
+
+//API METHOD TO EXTRACT FAVORITE NEWS
+app.get('/News/:email', (req, res) => {
+
+    const {email} = req.query;
+    connection.query("Call extract_news(?)", email, (err, results) =>{
+        if (err) {
+            return res.send(err)
+        } else{
+            return res.json({
+                data: results
+            })
+        }
+    });
+});
+
+
+//API METHOD TO ADD USER
+app.post('/Users/new', (req, res) =>{
+    const{email, password, idUserArticle  } = req.query;
+    connection.query("Call insert_user(?,?,?)", [email, password, idUserArticle], (err, results) =>{
+        if (err) {
+            return res.send(err)
+        } else{
+            return res.json({
+                data: results
+            })
+        }
+    })
+  
+
+});
+
 app.listen(5000, () => {
-    console.log('Server running on port 5000...')
+    console.log('Starting server on port 5000...');
+     
 })
